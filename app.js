94aab15fa3a8943811216750d279354af6f19245
Ste2038 <giulianelliste@gmail.com>
2018-03-08 11:48:52 +0100

    aggiunto metodo POST

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const basicAuth = require('express-basic-auth');
+var express = require('express');
+var connect = require('connect');
+var bodyParser = require('body-parser')
+var basicAuth = require('express-basic-auth');
 var app = express();
+var app1 = connect();
 //const Constants = require('./constants');
 
 app.use(basicAuth({
     users: { 'admin': 'secret'}
 }));
 
+app.use(bodyParser());
+
 app.get('/', function (req, res) {
+    console.log('GET /');
     res.send('Door Opened!');
     /*
     var request = require('xhr-request')
@@ -26,6 +32,28 @@ app.get('/', function (req, res) {
     })
     */
     console.log('Door Opened!');
+});
+
+app.post('/', function(req, res){
+    console.log('POST /');
+    res.send('Door Opened!');
+    /*
+    var request = require('xhr-request')
+    request(Constants.DoorIp, {
+        method: Constants.DoorMethod,
+        headers: {'Authorization' : Constants.HeadersValue}},
+        function (err, data, response) {
+        if (err) throw err  
+        if (response.statusCode == 200){
+            console.log("La porta è stata aperta!");
+        }
+
+        else {
+            console.log("C'è stato un errore, riprova!");
+        }
+    })
+    */
+   console.log('Door Opened!');
 });
 
 app.listen(process.env.PORT || 3000, function () {
